0ec9bb48d26628bb40f775b573765e40613237f3
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-10-14 10:34:37 +0300

    init

--- Layers/ContentView.swift
+++ Layers/ContentView.swift
@@ -10,7 +10,37 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+        VStack(spacing: 40) {
+            Text("The Layers")
+                .font(.largeTitle)
+            
+            Text("The Basics")
+                .foregroundColor(.gray)
+            
+            Text("With SwiftUI views, you can add layers on top (.overlay) and behind (.background) the view")
+                .frame(maxWidth: .infinity)
+                .padding()
+                .background(Color.blue)
+                .foregroundColor(Color.white)
+                .layoutPriority(1)
+            
+            Image("yosemite") // Show an image from Assets.xcassets
+                .resizable()
+                .scaledToFit()
+                .frame(width: 200, height: 200)
+                .opacity(0.7) // Make image only 70% solid
+                .background(Color.red.opacity(0.3)) // Layer behind image
+                .background(Color.yellow.opacity(0.3)) // Layer behind red
+                .background(Color.blue.opacity(0.3)) // Layer behind yellow
+                .overlay(Text("The Yosemite")
+                    .foregroundColor(Color.white)) // Layer on top image
+            
+            Image("Layers")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 200, height: 200)
+            
+        }.font(.title) // Apply this font style to all text in this container view
     }
 }
 
